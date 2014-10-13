01c6acda4673c30b02be8592ea2de744e5f0a30c
Rabie habachi <rabiie.habachi@gmail.com>
2014-10-13 19:25:27 +0100

    ouverture du layer

--- TpMapWinGis1/Form1.Designer.vb
+++ TpMapWinGis1/Form1.Designer.vb
@@ -22,9 +22,44 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxMap1 = New AxMapWinGIS.AxMap()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.AxMap1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxMap1
+        '
+        Me.AxMap1.Enabled = True
+        Me.AxMap1.Location = New System.Drawing.Point(268, 13)
+        Me.AxMap1.Name = "AxMap1"
+        Me.AxMap1.OcxState = CType(resources.GetObject("AxMap1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxMap1.Size = New System.Drawing.Size(421, 256)
+        Me.AxMap1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(786, 78)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(865, 297)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.AxMap1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.AxMap1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents AxMap1 As AxMapWinGIS.AxMap
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
--- TpMapWinGis1/Form1.vb
+++ TpMapWinGis1/Form1.vb
@@ -1,3 +1,26 @@
-﻿Public Class Form1
+﻿Imports MapWinGIS
+Public Class Form1
 
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+       
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim sh As New Shapefile
+        Dim odf As New OpenFileDialog
+
+        If odf.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Dim ouvert As Boolean = sh.Open(odf.FileName)
+            If ouvert Then
+                AxMap1.AddLayer(sh, True)
+                AxMap1.ZoomToMaxExtents()
+            Else
+                MessageBox.Show("le fichier est endommagé")
+            End If
+        End If
+        
+
+    End Sub
 End Class
